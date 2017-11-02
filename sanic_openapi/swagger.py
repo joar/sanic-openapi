c05e2a6388fbac6ba97157fe2a686500166e0f0b
Joar Wandborg <joar@wandborg.se>
2017-11-02 15:46:31 +0100

    Redirect /swagger ⇒ /swagger/

--- sanic_openapi/swagger.py
+++ sanic_openapi/swagger.py
@@ -1,11 +1,20 @@
 import os
 
+from sanic import response
 from sanic.blueprints import Blueprint
+from sanic.request import Request
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 dir_path = os.path.abspath(dir_path + '/ui')
 
-blueprint = Blueprint('swagger', url_prefix='swagger')
+blueprint = Blueprint('swagger', url_prefix='swagger', strict_slashes=True)
 
 blueprint.static('/', dir_path + '/index.html')
 blueprint.static('/', dir_path)
+
+
+@blueprint.get('')
+def redirect_slash(request: Request):
+    return response.redirect(request.path + '/')
+
+
